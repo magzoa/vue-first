eb0df8b6ab98aa82e18c8d8922707612364612d1
magzoaa <magzoaa@gmail.com>
2019-10-06 01:11:50 -0400

    Primer commit

---
+++ src/services/produtos.js
@@ -0,0 +1,14 @@
+import {http} from './config';
+
+
+export default{
+
+    listar:()=>{
+        return http.get('produtos');
+    },
+    salvar:(produto)=>{
+        return http.post('produto',produto)
+    }
+
+
+}
